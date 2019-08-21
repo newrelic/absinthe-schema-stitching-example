ac2e2d216174dfc1000b75501e717a3a591a03c6
caitlinhalla <caitlinhalla@gmail.com>
2019-08-21 14:12:28 -0700

    add query generator and query generator tests

---
+++ apps/schema_stitch/lib/query_generator.ex
@@ -0,0 +1,291 @@
+defmodule SchemaStitch.QueryGenerator do
+  @moduledoc """
+  Takes an Absinthe resolution struct and generates the relevant GraphQL query and variables.
+  """
+  alias Absinthe.Blueprint
+
+  @typename_field %Blueprint.Document.Field{name: "__typename"}
+
+  def render(%{definition: tree, path: path, fragments: fragments}) do
+    root_operation_node = List.last(path)
+
+    used_fragments = gather_fragments(tree, fragments)
+    used_variables = gather_variables(tree, used_fragments)
+
+    query = build_query(tree, root_operation_node, used_variables, used_fragments)
+
+    {query, used_variables}
+  end
+
+  defp gather_fragments(tree, fragments) do
+    used_fragments = gather_tree_fragments(tree) ++ gather_nested_fragments(fragments)
+
+    Enum.filter(fragments, fn {name, _fragment} -> name in used_fragments end)
+    |> Enum.into(%{})
+  end
+
+  defp gather_tree_fragments(%{selections: selections}) do
+    Enum.flat_map(selections, &gather_tree_fragments(&1))
+  end
+
+  defp gather_tree_fragments(%Blueprint.Document.Fragment.Spread{name: name}) do
+    [name]
+  end
+
+  defp gather_nested_fragments(fragments) do
+    Enum.flat_map(fragments, fn {_name, fragment} -> gather_tree_fragments(fragment) end)
+  end
+
+  def gather_variables(tree, fragments) do
+    (gather_tree_variables(tree) ++ gather_fragment_variables(fragments))
+    |> Enum.into(%{})
+  end
+
+  defp gather_tree_variables(%Blueprint.Document.Field{arguments: arguments, selections: []}) do
+    gather_arguments(arguments)
+  end
+
+  defp gather_tree_variables(%Blueprint.Document.Field{
+         arguments: arguments,
+         selections: selections
+       }) do
+    argument_names = gather_arguments(arguments)
+
+    Enum.reduce(selections, argument_names, fn selection, acc ->
+      acc ++ gather_tree_variables(selection)
+    end)
+  end
+
+  defp gather_tree_variables(%{selections: selections}) do
+    Enum.reduce(selections, [], fn selection, acc ->
+      acc ++ gather_tree_variables(selection)
+    end)
+  end
+
+  defp gather_tree_variables(_) do
+    []
+  end
+
+  defp gather_fragment_variables(fragments) do
+    Enum.reduce(fragments, [], fn {_name, fragment}, acc ->
+      acc ++ gather_tree_variables(fragment)
+    end)
+  end
+
+  def gather_arguments(arguments) do
+    arguments
+    |> Enum.map(&select_variable_arguments/1)
+    |> List.flatten()
+    |> Enum.reject(&is_nil/1)
+  end
+
+  defp select_variable_arguments(%{
+         input_value: %{
+           raw: %Blueprint.Input.RawValue{content: %Blueprint.Input.Variable{name: name}},
+           data: data
+         }
+       }) do
+    {name, data}
+  end
+
+  defp select_variable_arguments(%{
+         input_value: %{
+           normalized: %Absinthe.Blueprint.Input.Object{fields: fields}
+         }
+       }) do
+    Enum.map(fields, &select_variable_arguments(&1))
+  end
+
+  defp select_variable_arguments(_argument) do
+    nil
+  end
+
+  @indent_increment 2
+  defp build_query(tree, root_operation_node, used_variables, fragments) do
+    root_operation_type = build_operation_type(root_operation_node)
+    root_operation_name = build_operation_name(root_operation_node)
+
+    root_operation_variables = build_operation_variables(root_operation_node, used_variables)
+
+    field_selection = build_field_selection(tree, @indent_increment)
+    spread_fragments = build_fragments(fragments)
+
+    "#{root_operation_type}#{root_operation_name}#{root_operation_variables} " <>
+      "{\n#{field_selection}\n}#{spread_fragments}"
+  end
+
+  defp build_operation_type(%Blueprint.Document.Operation{type: operation_type}) do
+    "#{operation_type}"
+  end
+
+  defp build_operation_name(%Blueprint.Document.Operation{name: nil}) do
+    ""
+  end
+
+  defp build_operation_name(%Blueprint.Document.Operation{name: operation_name}) do
+    " #{operation_name}"
+  end
+
+  defp build_operation_variables(
+         %Blueprint.Document.Operation{variable_definitions: variable_definitions},
+         used_variables
+       ) do
+    used_variable_names = Map.keys(used_variables)
+
+    variable_definitions
+    |> Enum.filter(&(&1.name in used_variable_names))
+    |> Enum.map(&build_operation_variable_definition/1)
+    |> Enum.join(", ")
+    |> case do
+      "" -> ""
+      variables -> "(#{variables})"
+    end
+  end
+
+  defp build_operation_variable_definition(%Blueprint.Document.VariableDefinition{
+         name: name,
+         type: type
+       }) do
+    "$#{name}: #{build_variable_typename(type)}"
+  end
+
+  defp build_variable_typename(%Blueprint.TypeReference.NonNull{of_type: of_type}) do
+    build_variable_typename(of_type) <> "!"
+  end
+
+  defp build_variable_typename(%Blueprint.TypeReference.List{of_type: of_type}) do
+    "[" <> build_variable_typename(of_type) <> "]"
+  end
+
+  defp build_variable_typename(%Blueprint.TypeReference.Name{name: typename}) do
+    typename
+  end
+
+  defp build_field_selection(
+         %Blueprint.Document.Fragment.Inline{
+           selections: children,
+           schema_node: %{name: type_name}
+         },
+         indent_level
+       ) do
+    indent = String.duplicate(" ", indent_level)
+
+    subtree =
+      Enum.map(children, &build_field_selection(&1, indent_level + @indent_increment))
+      |> Enum.join("\n")
+
+    "#{indent}... on #{type_name} {\n#{subtree}\n#{indent}}"
+  end
+
+  defp build_field_selection(
+         %Blueprint.Document.Field{selections: []} = blueprint_node,
+         indent_level
+       ) do
+    String.duplicate(" ", indent_level) <> build_field_name(blueprint_node)
+  end
+
+  defp build_field_selection(
+         %Blueprint.Document.Field{selections: children} = blueprint_node,
+         indent_level
+       ) do
+    indent = String.duplicate(" ", indent_level)
+    field_name = build_field_name(blueprint_node)
+
+    subtree =
+      [@typename_field | children]
+      |> Enum.map(&build_field_selection(&1, indent_level + @indent_increment))
+      |> Enum.join("\n")
+
+    "#{indent}#{field_name} {\n#{subtree}\n#{indent}}"
+  end
+
+  defp build_field_selection(
+         %Blueprint.Document.Fragment.Spread{name: fragment_name},
+         indent_level
+       ) do
+    indent = String.duplicate(" ", indent_level)
+    "#{indent}...#{fragment_name}"
+  end
+
+  defp build_field_name(%Blueprint.Document.Field{name: name, arguments: args}) do
+    args
+    |> exclude_args_with_default_values
+    |> case do
+      [] -> name
+      used_input_args -> "#{name}(#{build_input_args(used_input_args)})"
+    end
+  end
+
+  defp exclude_args_with_default_values(args) do
+    Enum.reject(args, fn
+      %{
+        input_value: %Blueprint.Input.Value{
+          normalized: %Absinthe.Blueprint.Input.Generated{}
+        }
+      } ->
+        true
+
+      _ ->
+        false
+    end)
+  end
+
+  defp build_input_args(args) do
+    Enum.map(args, &build_input_arg(&1)) |> Enum.join(", ")
+  end
+
+  defp build_input_arg(%{
+         name: arg_name,
+         input_value: input_value
+       }) do
+    "#{arg_name}: #{build_input_value(input_value)}"
+  end
+
+  defp build_input_value(%Blueprint.Input.Value{
+         raw: %Blueprint.Input.RawValue{content: %Blueprint.Input.Variable{name: var_name}}
+       }) do
+    "$#{var_name}"
+  end
+
+  defp build_input_value(%Blueprint.Input.Value{
+         normalized: %Blueprint.Input.String{value: value}
+       }) do
+    "\"#{value}\""
+  end
+
+  defp build_input_value(%Blueprint.Input.Value{
+         normalized: %Blueprint.Input.Object{fields: sub_fields}
+       }) do
+    "{ #{build_input_args(sub_fields)} }"
+  end
+
+  defp build_input_value(%Blueprint.Input.Value{
+         normalized: %Blueprint.Input.List{items: items}
+       }) do
+    list_items = Enum.map(items, &build_input_value/1) |> Enum.join(", ")
+    "[ #{list_items} ]"
+  end
+
+  defp build_input_value(%Blueprint.Input.Value{
+         normalized: %Blueprint.Input.Null{}
+       }) do
+    "null"
+  end
+
+  defp build_input_value(%Blueprint.Input.Value{normalized: %{value: value}}) do
+    value
+  end
+
+  defp build_fragments(fragments) do
+    Enum.map(fragments, &build_fragment(&1))
+    |> Enum.join("")
+  end
+
+  defp build_fragment(
+         {fragment_name, %{type_condition: %{name: type_name}, selections: children}}
+       ) do
+    subtree = Enum.map(children, &build_field_selection(&1, @indent_increment)) |> Enum.join("\n")
+
+    "\n\nfragment #{fragment_name} on #{type_name} {\n#{subtree}\n}"
+  end
+end
---
+++ apps/schema_stitch/test/query_generator_test.exs
@@ -0,0 +1,519 @@
+defmodule SchemaStitch.QueryGeneratorTest do
+  use ExUnit.Case
+  require Logger
+
+  defmodule Schema do
+    use Absinthe.Schema
+
+    object :elected_official do
+      field(:id, :id)
+      field(:name, :string)
+      field(:office, :office)
+    end
+
+    object :office do
+      field(:name, :string)
+      field(:phone, :string)
+
+      field(:address, :address) do
+        arg(:type, :string)
+      end
+    end
+
+    object :address do
+      field(:street, :string)
+      field(:city, :string)
+      field(:state, :string)
+      field(:zipcode, :string)
+    end
+
+    input_object :school_input_object do
+      field(:grade_levels, list_of(:integer))
+      field(:zipcode, :string)
+    end
+
+    input_object :farmers_market_input_object do
+      field(:neighborhood, :string)
+      field(:day_of_the_week, :string)
+    end
+
+    query do
+      field :city_name, :string do
+        resolve(fn _, _, _ ->
+          {:ok, "Portland!"}
+        end)
+      end
+
+      field(:emergency_phone_number, :string) do
+        resolve(&send_resolution/2)
+      end
+
+      field(:neighborhoods, list_of(:string)) do
+        resolve(&send_resolution/2)
+      end
+
+      field :mayor, :elected_official do
+        resolve(&send_resolution/2)
+      end
+
+      field :city_council, list_of(:elected_official) do
+        resolve(&send_resolution/2)
+      end
+
+      field :county_name, :string do
+        arg(:zipcode, non_null(:string))
+        resolve(&send_resolution/2)
+      end
+
+      field :food_cart_names, list_of(:string) do
+        arg(:neighborhoods, non_null(list_of(:string)))
+        resolve(&send_resolution/2)
+      end
+
+      field :schools, list_of(:string) do
+        arg(:school_input_object, non_null(:school_input_object))
+        resolve(&send_resolution/2)
+      end
+
+      field :farmers_markets, list_of(:string) do
+        arg(:farmers_market_input, non_null(list_of(:farmers_market_input_object)))
+        resolve(&send_resolution/2)
+      end
+    end
+
+    mutation do
+      field :update_food_cart_name, :string do
+        arg(:new_name, non_null(:string))
+
+        resolve(&send_resolution/2)
+      end
+    end
+
+    defp send_resolution(_, resolution) do
+      send(self(), {:resolution, resolution})
+      {:ok, nil}
+    end
+  end
+
+  describe "field selection" do
+    test "accessing a scalar" do
+      {generated_query, _used_vars} =
+        """
+        {
+          emergencyPhoneNumber
+        }
+        """
+        |> externalize()
+
+      assert generated_query ==
+               """
+               query {
+                 emergencyPhoneNumber
+               }
+               """
+               |> String.trim()
+    end
+
+    test "accessing an object" do
+      {generated_query, _used_vars} =
+        """
+        {
+          mayor {
+            id
+            name
+          }
+        }
+        """
+        |> externalize()
+
+      assert generated_query ==
+               """
+               query {
+                 mayor {
+                   __typename
+                   id
+                   name
+                 }
+               }
+               """
+               |> String.trim()
+    end
+  end
+
+  describe "field arguments" do
+    test "with scalar argument" do
+      {generated_query, _used_vars} =
+        """
+        {
+          countyName(zipcode: "97214")
+        }
+        """
+        |> externalize()
+
+      assert generated_query ==
+               """
+               query {
+                 countyName(zipcode: "97214")
+               }
+               """
+               |> String.trim()
+    end
+
+    test "with scalar argument supplied via variable" do
+      variables = %{"zipcode" => "97214"}
+
+      {generated_query, used_vars} =
+        """
+        query($zipcode: String!) {
+          countyName(zipcode: $zipcode)
+        }
+        """
+        |> externalize(variables)
+
+      assert generated_query ==
+               """
+               query($zipcode: String!) {
+                 countyName(zipcode: $zipcode)
+               }
+               """
+               |> String.trim()
+
+      assert used_vars == variables
+    end
+
+    test "with list of scalars argument" do
+      {generated_query, _used_vars} =
+        """
+        {
+          foodCartNames(neighborhoods: [ "Alberta Arts", "Hawthorne" ])
+        }
+        """
+        |> externalize()
+
+      assert generated_query ==
+               """
+               query {
+                 foodCartNames(neighborhoods: [ "Alberta Arts", "Hawthorne" ])
+               }
+               """
+               |> String.trim()
+    end
+
+    test "with an inline input object argument" do
+      {generated_query, _used_vars} =
+        """
+        {
+          schools(schoolInputObject: { gradeLevels: [ 1, 2 ], zipcode: "97211" })
+        }
+        """
+        |> externalize()
+
+      assert generated_query ==
+               """
+               query {
+                 schools(schoolInputObject: { gradeLevels: [ 1, 2 ], zipcode: "97211" })
+               }
+               """
+               |> String.trim()
+    end
+
+    test "with list of inline input objects argument" do
+      {generated_query, _used_vars} =
+        """
+        {
+          farmersMarkets(farmersMarketInput: [{ neighborhood: "Sellwood", dayOfTheWeek: "Monday" }, { neighborhood: "Multnomah Village", dayOfTheWeek: "Saturday" }])
+        }
+        """
+        |> externalize()
+
+      assert generated_query ==
+               """
+               query {
+                 farmersMarkets(farmersMarketInput: [ { neighborhood: "Sellwood", dayOfTheWeek: "Monday" }, { neighborhood: "Multnomah Village", dayOfTheWeek: "Saturday" } ])
+               }
+               """
+               |> String.trim()
+    end
+
+    test "with input object as variable" do
+      variables = %{
+        "farmersMarketInputObject" => [
+          %{
+            "neighborhood" => "Sellwood",
+            "dayOfTheWeek" => "Monday"
+          },
+          %{
+            "neighborhood" => "Multnomah Village",
+            "dayOfTheWeek" => "Saturday"
+          }
+        ]
+      }
+
+      {generated_query, used_vars} =
+        """
+         query($farmersMarketInputObject: [FarmersMarketInputObject!]) {
+          farmersMarkets(farmersMarketInput: $farmersMarketInputObject)
+        }
+        """
+        |> externalize(variables)
+
+      assert generated_query ==
+               """
+               query($farmersMarketInputObject: [FarmersMarketInputObject!]) {
+                 farmersMarkets(farmersMarketInput: $farmersMarketInputObject)
+               }
+               """
+               |> String.trim()
+
+      assert used_vars == %{
+               "farmersMarketInputObject" => [
+                 %{day_of_the_week: "Monday", neighborhood: "Sellwood"},
+                 %{day_of_the_week: "Saturday", neighborhood: "Multnomah Village"}
+               ]
+             }
+    end
+  end
+
+  describe "query fragments" do
+    test "with inline fragment" do
+      {generated_query, _used_vars} =
+        """
+        {
+          mayor {
+            ... on ElectedOfficial {
+              name
+            }
+          }
+        }
+        """
+        |> externalize()
+
+      assert generated_query ==
+               """
+               query {
+                 mayor {
+                   __typename
+                   ... on ElectedOfficial {
+                     name
+                   }
+                 }
+               }
+               """
+               |> String.trim()
+    end
+
+    test "with named fragment" do
+      {generated_query, _used_vars} =
+        """
+        {
+          mayor {
+            ... on ElectedOfficial {
+              name
+              ...officeFragment
+            }
+          }
+        }
+
+        fragment officeFragment on ElectedOfficial {
+          office {
+            name
+          }
+        }
+        """
+        |> externalize()
+
+      assert generated_query ==
+               """
+               query {
+                 mayor {
+                   __typename
+                   ... on ElectedOfficial {
+                     name
+                     ...officeFragment
+                   }
+                 }
+               }
+
+               fragment officeFragment on ElectedOfficial {
+                 office {
+                   __typename
+                   name
+                 }
+               }
+               """
+               |> String.trim()
+    end
+
+    test "with nested fragments" do
+      {generated_query, _used_vars} =
+        """
+        {
+          mayor {
+            ... on ElectedOfficial {
+              name
+              ...officeFragment
+            }
+          }
+        }
+
+        fragment officeFragment on ElectedOfficial {
+          office {
+            name
+            ...addressFragment
+          }
+        }
+
+        fragment addressFragment on Office {
+          address {
+            street
+            state
+            zipcode
+          }
+        }
+        """
+        |> externalize()
+
+      assert generated_query ==
+               """
+               query {
+                 mayor {
+                   __typename
+                   ... on ElectedOfficial {
+                     name
+                     ...officeFragment
+                   }
+                 }
+               }
+
+               fragment addressFragment on Office {
+                 address {
+                   __typename
+                   street
+                   state
+                   zipcode
+                 }
+               }
+
+               fragment officeFragment on ElectedOfficial {
+                 office {
+                   __typename
+                   name
+                   ...addressFragment
+                 }
+               }
+               """
+               |> String.trim()
+    end
+
+    test "with nested argument" do
+      variables = %{"type" => "work"}
+
+      {generated_query, used_vars} =
+        """
+        query($type: String) {
+          mayor {
+            ... on ElectedOfficial {
+              name
+              ...officeFragment
+            }
+          }
+        }
+
+        fragment officeFragment on ElectedOfficial {
+          office {
+            name
+            ...addressFragment
+          }
+        }
+
+        fragment addressFragment on Office {
+          address(type: $type) {
+            street
+            state
+            zipcode
+          }
+        }
+        """
+        |> externalize(variables)
+
+      assert generated_query ==
+               """
+               query($type: String) {
+                 mayor {
+                   __typename
+                   ... on ElectedOfficial {
+                     name
+                     ...officeFragment
+                   }
+                 }
+               }
+
+               fragment addressFragment on Office {
+                 address(type: $type) {
+                   __typename
+                   street
+                   state
+                   zipcode
+                 }
+               }
+
+               fragment officeFragment on ElectedOfficial {
+                 office {
+                   __typename
+                   name
+                   ...addressFragment
+                 }
+               }
+               """
+               |> String.trim()
+
+      assert variables == used_vars
+    end
+  end
+
+  test "a mutation" do
+    {generated_query, _used_vars} =
+      """
+      mutation {
+        updateFoodCartName(newName: "Matt's BBQ")
+      }
+      """
+      |> externalize()
+
+    assert generated_query ==
+             """
+             mutation {
+               updateFoodCartName(newName: "Matt's BBQ")
+             }
+             """
+             |> String.trim()
+  end
+
+  test "query non-forwarded field" do
+    {generated_query, _used_vars} =
+      """
+      {
+        cityName
+        emergencyPhoneNumber
+      }
+      """
+      |> externalize()
+
+    assert generated_query ==
+             """
+             query {
+               emergencyPhoneNumber
+             }
+             """
+             |> String.trim()
+  end
+
+  defp externalize(query_document, variables \\ nil) do
+    {:ok, response} = Absinthe.run(query_document, Schema, variables: variables)
+
+    assert response[:errors] == nil
+    assert_receive({:resolution, resolution})
+
+    SchemaStitch.QueryGenerator.render(resolution)
+  end
+end
